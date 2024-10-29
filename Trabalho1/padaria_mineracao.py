7d62e7d8461a39e4d5f89836cd2f6ff515760d68
Leandro Kise <leandrokise753@gmail.com>
2024-10-29 15:11:30 -0300

    Traduzindo variaveis para o portugues

--- Trabalho1/padaria_mineracao.py
+++ Trabalho1/padaria_mineracao.py
@@ -4,6 +4,9 @@ from mlxtend.frequent_patterns import apriori, association_rules
 import matplotlib.pyplot as plt
 import re
 
+def mostrar_df_one_hot(df, tipo):
+    print(f"\nDataFrame Final com Doce {tipo} One-Hot Encoding:")
+    print(f"{df}\n")
 
 def mostrar_grafico_ocorrencias(item_counts):
     plt.figure(figsize=(12, 10))
@@ -13,7 +16,7 @@ def mostrar_grafico_ocorrencias(item_counts):
     plt.title('Frequência de Itens')
     plt.xticks(rotation=20, ha='right')
     plt.show()
-    
+
 def formatar_produtos(produtos, tipo):
     produtos_formatados = []  # Lista para armazenar os produtos formatados
     for produto in produtos:
@@ -63,16 +66,16 @@ if 'produtos' in df.columns:
 
 # Mineração de Dados
 # 1. Explodindo a coluna de produtos
-df_exploded = df.explode('produtos')
-df_exploded_doce = df.explode('produtos2')
+df_explodir = df.explode('produtos')
+df_explodir_doce = df.explode('produtos2')
 
 # 2. Aplicando o one-hot encoding
-df_one_hot = pd.get_dummies(df_exploded['produtos'])
-df_one_hot_doce = pd.get_dummies(df_exploded_doce['produtos2'])
+df_one_hot = pd.get_dummies(df_explodir['produtos'])
+df_one_hot_doce = pd.get_dummies(df_explodir_doce['produtos2'])
 
 # 3. Agrupando e somando as ocorrências
-df_final = df_one_hot.groupby(df_exploded.index).sum()
-df_final2 = df_one_hot_doce.groupby(df_exploded_doce.index).sum()
+df_final = df_one_hot.groupby(df_explodir.index).sum()
+df_final2 = df_one_hot_doce.groupby(df_explodir_doce.index).sum()
 
 # 4. Convertendo os valores para True ou False
 df_final = df_final.astype(bool)
@@ -80,47 +83,45 @@ df_final2 = df_final2.astype(bool)
 
 # 4-1. Etapa para analisar se os dados estão corretos
 # Exibindo o Data Frame com One-Hot Encoding
-print("DataFrame Final com One-Hot Encoding:")
-print(df_final)
+mostrar_df_one_hot(df_final, "com Marca")
+mostrar_df_one_hot(df_final2, "Generico")
 
 # 5. Aplicando o Principio Apriori:
-frequent_itemsets = apriori(df_final, min_support=0.06, use_colnames=True)
-frequent_itemsets2 = apriori(df_final2, min_support=0.06, use_colnames=True)
+conjuntos_frequentes = apriori(df_final, min_support=0.06, use_colnames=True)
+conjuntos_frequentes2 = apriori(df_final2, min_support=0.06, use_colnames=True)
 
 # 6. Gerando regras de associação
-rules = association_rules(frequent_itemsets, metric="lift", min_threshold=0.6)
-rules2 = association_rules(frequent_itemsets2, metric="lift", min_threshold=0.6)
+regras = association_rules(conjuntos_frequentes, metric="lift", min_threshold=0.6)
+regras2 = association_rules(conjuntos_frequentes2, metric="lift", min_threshold=0.6)
 
 # 7. Mostrando Frequencia dos itens através de um gráfico
-item_counts = df_final.sum().sort_values(ascending=False)
-item_counts2 = df_final2.sum().sort_values(ascending=False)
-
-mostrar_grafico_ocorrencias(item_counts)
-mostrar_grafico_ocorrencias(item_counts2)
+contagem_itens = df_final.sum().sort_values(ascending=False)
+contagem_itens2 = df_final2.sum().sort_values(ascending=False)
+mostrar_grafico_ocorrencias(contagem_itens)
+mostrar_grafico_ocorrencias(contagem_itens2)
 
 # 8. Exibindo todas as regras de associação encontradas
 print("\nRegras de Associação:")
-print(f"\nTotal de regras de associação encontradas: {len(rules)}")
-print(rules[['antecedents', 'consequents', 'support',
+print(f"\nTotal de regras de associação encontradas: {len(regras)}")
+print(regras[['antecedents', 'consequents', 'support',
       'confidence', 'lift']].to_string(index=False))
 
 # 9. Exibir as 5 principais regras de associação
-top_rules = rules.sort_values('lift', ascending=False).head(5)
+top_regras = regras.sort_values('lift', ascending=False).head(5)
 print("\nAs 5 principais regras de associação:")
-print(top_rules[['antecedents', 'consequents', 'support',
+print(top_regras[['antecedents', 'consequents', 'support',
       'confidence', 'lift']].to_string(index=False))
 
 # 10. Exibir a regra mais influente
-max_lift_rule = rules.loc[rules['lift'].idxmax()]
+regra_maior_lift = regras.loc[regras['lift'].idxmax()]
 print("\nA regra mais influente (produto 1 para 1):")
-print(f"{list(max_lift_rule['antecedents'])} => {
-      list(max_lift_rule['consequents'])}")
-
+print(f"{list(regra_maior_lift['antecedents'])} => {
+      list(regra_maior_lift['consequents'])}")
 
 # 11. Exibir as regras que implicam Doce
-rules_with_doce = rules2[rules2['consequents'].apply(lambda x: 'Doce' in x)]
+regras_com_doce = regras2[regras2['consequents'].apply(lambda x: 'Doce' in x)]
 print("\nRegras que implicam a compra de 'Doce':")
-print(rules_with_doce[['antecedents', 'consequents',
+print(regras_com_doce[['antecedents', 'consequents',
       'support', 'confidence', 'lift']].to_string(index=False))
 
-input()  # esperar tecla
+input()  # Esperar tecla
